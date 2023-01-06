ff8d8cbd5b90446620dec306b204aa4534b8637e
AmaanKhanPathan <amaankhan5071@gmail.com>
2023-01-06 23:54:40 +0530

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,50 @@
+const selectImage = document.getElementById("selectImage");
+
+const all = Array.from(document.querySelectorAll(".all"));
+// const orange = Array.from(document.querySelectorAll(".orange"));
+// const blueviolet = Array.from(document.querySelectorAll(".blueviolet"));
+// const red = Array.from(document.querySelectorAll(".red"))
+// const yellowgreen = Array.from(document.querySelectorAll("yellowgreen"));
+
+const onSelectImage = (eve) => {
+   
+
+    let getClass = "."+eve.target.value;
+    console.log(getClass);
+    
+    all.forEach(div => div.classList.add('d-none'));
+
+    let getDivs = Array.from(document.querySelectorAll(getClass));
+    
+    getDivs.forEach(div => div.classList.remove('d-none'));
+        
+    
+        
+}
+
+selectImage.addEventListener('change', onSelectImage)
+
+
+
+
+
+// var cl = console.log;
+// //alert("hello")
+
+// const roseflower = document.getElementById("roseflower");
+// cl(roseflower);
+
+// const all = Array.from(document.querySelectorAll(".all"));
+// cl(all);
+
+// const onchangeimg = (eve) => {
+//   let getimg = eve.target.value;
+//   cl(getimg);
+//   all.forEach(div => div.classList.add("d-none"));
+
+//   let getdivs = Array.from(document.querySelectorAll("." + getimg));
+//   getdivs.forEach(div => div.classList.remove("d-none"));
+
+// }
+// roseflower.addEventListener("change", onchangeimg);
+
